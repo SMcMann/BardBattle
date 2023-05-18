29799e4b49d6fe92eed23b35712c83c5c9cbd5e7
CoolTomKGuy <54568685+CoolTomKGuy@users.noreply.github.com>
2023-05-17 19:17:58 -0700

    Controller Setup

--- Assets/MainMenus.cs
+++ Assets/MainMenus.cs
@@ -6,38 +6,40 @@ using UnityEngine.InputSystem;
 
 public class MainMenus : MonoBehaviour
 {
-    PlayerControls controls;
+    PlayerControls controlz;
+    public static bool startcontrols = true;
     public static bool p1tiefling = false;
     public static bool p1goblin = false;
     public static bool p2tiefling = false;
-    public static bool p2goblin = false;
-
+    public static bool p2goblin = false;   
     void Awake()
     {
-        controls = new PlayerControls();
-        controls.Gameplay.ButtonNorth.performed += ctx => PlayGame();
-        controls.Gameplay.ButtonWest.performed += ctx => PlayGame();
-        controls.Gameplay.ButtonSouth.performed += ctx => PlayGame();
-        controls.Gameplay.ButtonEast.performed += ctx => PlayGame();
+        startcontrols = true;
+        VictoryRestart.VictoryControls = false;
+        controlz = new PlayerControls();
+        controlz.Gameplay.ButtonNorth.performed += ctx => PlayGame();
+        controlz.Gameplay.ButtonWest.performed += ctx => PlayGame();
+        controlz.Gameplay.ButtonSouth.performed += ctx => PlayGame();
+        controlz.Gameplay.ButtonEast.performed += ctx => PlayGame();
+        if (startcontrols == true)
+        {
+            controlz.Gameplay.Enable();
+        }
+        else
+        {
+            controlz.Gameplay.Disable();
+        }
     }
     void OnEnable()
     {
-        controls.Gameplay.Enable();
+        controlz.Gameplay.Enable();
     }
     public void  PlayGame ()
     {
-        p1goblin = true;
-        p2tiefling = true;
-        Debug.Log(p2goblin);
-        Debug.Log(p2tiefling);
-        SceneManager.LoadScene("CharacterSelect");
-        
-    }
-    public void PlayGame2()
-    {
-        
-        
-        SceneManager.LoadScene("Testmap");
-
+        if (startcontrols == true)
+        {
+            controlz.Gameplay.Disable();
+            SceneManager.LoadScene("CharacterSelect");
+        }
     }
 }
