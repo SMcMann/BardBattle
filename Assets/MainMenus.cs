c7fa5fb86ba5b03e29a47b40c0192ca2f534cd2b
CoolTomKGuy <54568685+CoolTomKGuy@users.noreply.github.com>
2023-05-17 18:05:22 -0700

    Start Game with Controller

--- Assets/MainMenus.cs
+++ Assets/MainMenus.cs
@@ -2,13 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MainMenus : MonoBehaviour
 {
+    PlayerControls controls;
     public static bool p1tiefling = false;
     public static bool p1goblin = false;
     public static bool p2tiefling = false;
     public static bool p2goblin = false;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Gameplay.ButtonNorth.performed += ctx => PlayGame();
+        controls.Gameplay.ButtonWest.performed += ctx => PlayGame();
+        controls.Gameplay.ButtonSouth.performed += ctx => PlayGame();
+        controls.Gameplay.ButtonEast.performed += ctx => PlayGame();
+    }
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
     public void  PlayGame ()
     {
         p1goblin = true;
